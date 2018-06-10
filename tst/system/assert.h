1fa2ec875af272098ea19a186363af7452f7b774
Hemmo Nieminen <hemmo.nieminen@iki.fi>
2018-06-10 19:35:54 +0300

    Update README to point to GitLab.
    Signed-off-by: Hemmo Nieminen <hemmo.nieminen@iki.fi>

--- tst/system/assert.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include_next <assert.h>
-
-#ifdef UNIT_TESTING
-#include "cmocka-wrapper.h"
-#endif
--- tst/system/stdlib.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include_next <stdlib.h>
-
-#ifdef UNIT_TESTING
-#include "cmocka-wrapper.h"
-#endif
